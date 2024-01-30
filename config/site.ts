845b65005720c463198998d8063acdbb7360667c
Eka Prasetia <ekaone3033@gmail.com>
2024-01-30 21:14:32 +0700

    first commit

---
+++ config/site.ts
@@ -0,0 +1,17 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Eka Prasetia",
+  description: "Curiosity rover, read widely, try new things",
+  mainNav: [
+    {
+      title: "Contact",
+      href: "mailto:ekaone3033@gmail.com",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/twekaone",
+    github: "https://github.com/ekaone",
+    projects: "https://prasetia.me",
+  },
+};
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.domain.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
